8f335a0e3eceff82e9732d308a7b656afe030475
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2022-02-22 17:35:14 +0000

    Adicionando arquivo

---
+++ Pessoa.java
@@ -0,0 +1,34 @@
+public class Pessoa(){
+        private String nome;
+        private String endereco;
+        private Int telefone;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }      
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public Int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(Int telefone) {
+        this.telefone = telefone;
+    }
+
+
+
+}
+  public void setNome(String nome){
+      this.nome=nome;
+      blic String getNome(){
+          return this.nome;
+          
+      }
+}
